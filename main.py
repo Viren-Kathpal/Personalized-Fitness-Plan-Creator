9824042cfe7a89885decab0deaabaf48bb2cf1bf
Viren Kathpal <105204147+Viren-Kathpal@users.noreply.github.com>
2024-05-25 20:43:36 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-# Import the main function from the gui module
-from gui import main
-
-# Check if this script is being run directly (i.e., not being imported)
-if __name__ == "__main__":
-    # If true, call the main function to start the application
-    main()
